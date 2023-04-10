23dd06f8e8616d25c0b7948681848beb3c97f28d
Lina_Herrera <lina_h64@hotmail.com>
2023-04-10 18:54:57 -0500

    arreglando pag de peticiones y css

--- otraForma/peticiones.js
+++ otraForma/peticiones.js
@@ -1,3 +1,22 @@
+
+// const botonGet = document.getElementById('get');
+// botonGet.addEventListener('click', () => {
+//     const productosDiv = document.getElementById('productos');
+
+//     fetch('http://localhost:3031/productos')
+//         .then(response => response.json())
+//         .then(data => {
+//             for (let i = 0; i < data.length; i++) {
+//                 const producto = data[i];
+//                 const productoDiv = document.createElement('div');
+//                 productoDiv.classList.add('producto');
+//                 productoDiv.textContent = `Nombre: ${producto.nombre}, Precio: ${producto.precio}, Unidades: ${producto.unidades}, Categoría: ${producto.categoria}, Descripción: ${producto.descripcion}`;
+//                 productosDiv.appendChild(productoDiv);
+//             }
+//         })
+//         .catch(error => console.error(error));
+// });
+
 const botonGet = document.getElementById('get');
 botonGet.addEventListener('click', () => {
     const productosDiv = document.getElementById('productos');
@@ -5,15 +24,36 @@ botonGet.addEventListener('click', () => {
     fetch('http://localhost:3031/productos')
         .then(response => response.json())
         .then(data => {
-            const listaProductos = document.createElement('ul');
-
             data.forEach(producto => {
-                const itemProducto = document.createElement('li');
-                itemProducto.textContent = `Nombre: ${producto.nombre}, Precio: ${producto.precio}, Unidades: ${producto.unidades}, Categoría: ${producto.categoria}, Descripción: ${producto.descripción}`;
-                listaProductos.appendChild(itemProducto);
-            });
+                const productoDiv = document.createElement('div');
+                productoDiv.classList.add('producto');
 
-            productosDiv.appendChild(listaProductos);
+                const listaProducto = document.createElement('ul');
+                listaProducto.classList.add('lista-producto');
+
+                const nombreProducto = document.createElement('li');
+                nombreProducto.textContent = `Nombre: ${producto.nombre}`;
+                listaProducto.appendChild(nombreProducto);
+
+                const precioProducto = document.createElement('li');
+                precioProducto.textContent = `Precio: ${producto.precio}`;
+                listaProducto.appendChild(precioProducto);
+
+                const unidadesProducto = document.createElement('li');
+                unidadesProducto.textContent = `Unidades: ${producto.unidades}`;
+                listaProducto.appendChild(unidadesProducto);
+
+                const categoriaProducto = document.createElement('li');
+                categoriaProducto.textContent = `Categoría: ${producto.categoria}`;
+                listaProducto.appendChild(categoriaProducto);
+
+                const descripcionProducto = document.createElement('li');
+                descripcionProducto.textContent = `Descripción: ${producto.descripción}`;
+                listaProducto.appendChild(descripcionProducto);
+
+                productoDiv.appendChild(listaProducto);
+                productosDiv.appendChild(productoDiv);
+            });
         })
         .catch(error => console.error(error));
 });
