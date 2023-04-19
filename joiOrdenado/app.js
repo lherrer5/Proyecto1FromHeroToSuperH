b119dab4810cfea45a3a89faa7dfb643b7930f6a
Lina_Herrera <lina_h64@hotmail.com>
2023-04-19 12:36:24 -0500

    inicio validación para creación de archivo productsOrd.txt en peticiones

--- joiOrdenado/app.js
+++ joiOrdenado/app.js
@@ -10,6 +10,11 @@ const PORT = 3060;
 app.use(express.json());
 app.use(cors());
 
+// Comprobar si el archivo existe, si no existe, crearlo con un arreglo vacío
+if (!fs.existsSync("./productosOrd.txt")) {
+    fs.writeFileSync("./productosOrd.txt", "");
+}
+
 // Obtener todos los productos
 app.get("/productos", (req, res) => {
     fs.readFile("productosOrd.txt", "utf8", (err, data) => {
@@ -58,7 +63,13 @@ app.post("/productos", (req, res) => {
             res.status(500).send("Error al agregar el producto");
             return;
         }
-        const productos = data.trim().split("\n").map((producto) => JSON.parse(producto));
+        //validación del archivo productsOrd.txt vacio
+        let productos = [];
+        if (data) {
+            productos = data.trim().split("\n").map((producto) => JSON.parse(producto));
+        }
+
+        //const productos = data.trim().split("\n").map((producto) => JSON.parse(producto));
         const maxId = productos.reduce((max, producto) => {
             return producto.id > max ? producto.id : max;
         }, 0);
@@ -100,7 +111,7 @@ app.patch("/productos/:id", (req, res) => {
             return;
         }
         let productos = data.trim().split("\n").map((producto) => JSON.parse(producto));
-        const productoIndex = productos.findIndex((producto) => producto.id.toString() === id);
+        const productoIndex = productos.findIndex((producto) => producto.id.toString() == id);
         if (productoIndex === -1) {
             res.status(404).send("Producto no encontrado");
             return;
@@ -140,7 +151,7 @@ app.delete("/productos/:id", (req, res) => {
             return;
         }
         const productos = data.trim().split("\n").map((producto) => JSON.parse(producto));
-        const indiceProducto = productos.findIndex((producto) => producto.id.toString() === id);
+        const indiceProducto = productos.findIndex((producto) => producto.id.toString() == id);
         if (indiceProducto === -1) {
             res.status(404).send("Producto no encontrado");
             return;
@@ -158,60 +169,32 @@ app.delete("/productos/:id", (req, res) => {
     });
 });
 
-
-// app.delete("/productos/:id", (req, res) => {
-//     const id = req.params.id;
+// Eliminar un producto existente con NOMBRE
+// app.delete("/productos/:nombre", (req, res) => {
+//     const nombre = req.params.nombre;
 //     fs.readFile("productosOrd.txt", "utf8", (err, data) => {
 //         if (err) {
 //             console.error(err);
-//             res.status(500).send("Error al eliminar el producto");
+//             res.status(500).send("Error al obtener los productos");
 //             return;
 //         }
-//         const productos = data.trim().split("\n").map((producto) => JSON.parse(producto));
-//         const indiceProducto = productos.findIndex((producto) => producto.id.toString() === id);
-//         if (indiceProducto === -1) {
+//         let productos = data.trim().split("\n").map(JSON.parse);
+//         const productoIndex = productos.findIndex(p => p.nombre === nombre);
+//         if (productoIndex !== -1) {
+//             productos.splice(productoIndex, 1);
+//             fs.writeFile("productosOrd.txt", productos.map(JSON.stringify).join("\n") + "\n", "utf8", err => {
+//                 if (err) {
+//                     console.error(err);
+//                     res.status(500).send("Error al eliminar el producto");
+//                     return;
+//                 }
+//                 res.status(204).send();
+//             });
+//         } else {
 //             res.status(404).send("Producto no encontrado");
-//             return;
 //         }
-//         const productoEliminado = productos.splice(indiceProducto, 1)[0];
-//         fs.writeFile("productosOrd.txt", productos.map((producto) => JSON.stringify(producto)).join("\n"), "utf8", (err) => {
-//             if (err) {
-//                 console.error(err);
-//                 res.status(500).send("Error al eliminar el producto");
-//                 return;
-//             }
-//             res.json(productoEliminado);
-//         }
-//         );
 //     });
 // });
-
-// Eliminar un producto existente con NOMBRE
-app.delete("/productos/:nombre", (req, res) => {
-    const nombre = req.params.nombre;
-    fs.readFile("productosOrd.txt", "utf8", (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send("Error al obtener los productos");
-            return;
-        }
-        let productos = data.trim().split("\n").map(JSON.parse);
-        const productoIndex = productos.findIndex(p => p.nombre === nombre);
-        if (productoIndex !== -1) {
-            productos.splice(productoIndex, 1);
-            fs.writeFile("productosOrd.txt", productos.map(JSON.stringify).join("\n") + "\n", "utf8", err => {
-                if (err) {
-                    console.error(err);
-                    res.status(500).send("Error al eliminar el producto");
-                    return;
-                }
-                res.status(204).send();
-            });
-        } else {
-            res.status(404).send("Producto no encontrado");
-        }
-    });
-});
 
 
 // Iniciar el servidor
