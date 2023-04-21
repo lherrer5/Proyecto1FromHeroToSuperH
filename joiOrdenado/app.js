47dee4656e4b19d6beee588439171b288c6bfd67
Lina_Herrera <lina_h64@hotmail.com>
2023-04-21 11:44:50 -0500

    archvo app.js de joiOrdenado comentado

--- joiOrdenado/app.js
+++ joiOrdenado/app.js
@@ -10,7 +10,7 @@ const PORT = 3060;
 app.use(express.json());
 app.use(cors());
 
-// Comprobar si el archivo existe, si no existe, crearlo con un arreglo vacío
+// Comprobar si el archivo existe, si no, se crea con un array vacío
 if (!fs.existsSync("./productosOrd.txt")) {
     fs.writeFileSync("./productosOrd.txt", "[]");
 }
@@ -25,13 +25,19 @@ app.get("/productos", (req, res) => {
         }
         //validación archivo vacio
         if (!data || data.trim() === "") {
+//hago validacion con .trim pa evitar q "" sea leida como falso y se detecte error sin parar el servidor 
             res.json([]);
             return;
         }
+//creo el array q almacenará productos
         let productos = [];
         try {
+//si encuentra productos, los convierte a js y guarda en array pdts
             productos = JSON.parse(data.trim());
+//.trim pa eliminar los espacios al ppio/final d la cadena d datos JSON antes de analizarla con JSON.parse() 
+//garantizando q esta se analice correctamente como JSON y que el objeto de JS devuelto se use correctamente
         } catch (e) {
+// capturo error al parsear el JSON, lo imprimo y envio rpta de error con status 500 de server error y msj
             console.error(e);
             res.status(500).send("Error al obtener los productos");
             return;
@@ -70,12 +76,16 @@ app.post("/productos", (req, res) => {
         console.log(error);
         return res.status(400).send(error.details);
     }
-
+//Guardo el producto nuevo obtenido del body validado
     const nuevoProducto = value;
     fs.readFile("productosOrd.txt", "utf8", (err, data) => {
         if (err) {
             console.error(err);
             res.status(500).send("Error al agregar el producto");
+            return;
+        }
+        if (!data || data.trim() === "") {
+            res.status(404).send("Producto no encontrado");
             return;
         }
         let productos = [];
@@ -86,8 +96,11 @@ app.post("/productos", (req, res) => {
             res.status(500).send("Error al agregar el producto");
             return;
         }
+//reduce recorre los elementos del array y aplica una fnc q compara el id d c/producto con el valor máx acum y devuelve el mayor. Al final, maxId contendrá el valor máx del id en el array productos
         const maxId = productos.reduce((max, producto) => {
+//max es el valor acumulado actual (inicia en 0), y producto es el elemento actual del array.
             return producto.id > max ? producto.id : max;
+//Comparo el id del pdto actual con el valor max acum. Si producto.id es mayor que max, devuelve producto.id y continua iteracion; de lo contrario, devuelve max.
         }, 0);
         const idNuevoProducto = maxId + 1;
         const productoConId = {
@@ -106,7 +119,7 @@ app.post("/productos", (req, res) => {
                 res.status(500).send("Error al agregar el producto");
                 return;
             }
-            res.status(201).json(productoConId);
+            res.status(201).json(productoConId);//201 created
         });
     });
 });
@@ -125,18 +138,25 @@ app.patch("/productos/:id", (req, res) => {
             res.status(500).send("Error al actualizar el producto");
             return;
         }
+        if (!data || data.trim() === "") {
+            res.status(404).send("Producto no encontrado");
+            return;
+        }
         let productos = JSON.parse(data.trim());
-        const productoIndex = productos.findIndex((producto) => producto.id.toString() === id);
-        if (productoIndex === -1) {
+        const indiceProducto = productos.findIndex((producto) => producto.id.toString() === id);
+        if (indiceProducto === -1) {
             res.status(404).send("Producto no encontrado");
             return;
         }
         const productoActualizado = {
-            ...productos[productoIndex],
+// obtengo las propiedades actuales d mi pdto a actualizar
+            ...productos[indiceProducto],
+//y las reemplazo con los valores recibidos
             ...value,
         };
-        productos[productoIndex] = productoActualizado;
-
+// Actualizo el pdto en productos
+        productos[indiceProducto] = productoActualizado;
+//uso null y 2 para hacer más legible el json poniendo una sangría de 2 espacios, no es obligatorio
         const productoNuevo = JSON.stringify(productos, null, 2);
         fs.writeFile("productosOrd.txt", productoNuevo, (err) => {
             if (err) {
@@ -176,6 +196,7 @@ app.delete("/productos/:id", (req, res) => {
             res.status(404).send("Producto no encontrado");
             return;
         }
+ // 1 indica cuantos elemt se eliminaran y como splice() devuelve un array con el/los elemt eliminados, uso [0] pa obtener el 1er elemt eliminado.
         const productoEliminado = productos.splice(indiceProducto, 1)[0];
         fs.writeFile("productosOrd.txt", JSON.stringify(productos), "utf8", (err) => {
             if (err) {
